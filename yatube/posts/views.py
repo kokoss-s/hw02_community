5cdc425eefb11f390bdfc5cc15ce4a8ee4556186
Кирилл Черный <kirillcerny@gmail.com>
2022-11-21 09:45:06 +0300

    тесты пройдены

---
+++ yatube/posts/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к
+    # меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Это главная страница проекта Yatube'
+    template = 'posts/index.html'
+
+    context = {
+        'posts': posts,
+        'title': title,
+
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
+
+
+'''
+def group_posts(request, slug):
+
+    template = 'posts/group_list.html'
+
+    title = 'Здесь будет информация о группах проекта Yatube'
+
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
+'''
+
+'''
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
+'''
