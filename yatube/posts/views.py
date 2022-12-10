c0735c8212c7529d8db967787ec06903bffdf50f
Кирилл Черный <kirillcerny@gmail.com>
2022-12-10 18:50:37 +0300

    Паджинатор на главной странице и странице с постами + некоторые правки вьюхи и шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post, Group
 
 QUANTITY_OF_POSTS = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:QUANTITY_OF_POSTS]
-    title = 'Это главная страница проекта Yatube'
+
+    # получение списка постов в обратном порядке.
+    post_list = Post.objects.all().order_by('-pub_date')
+    # по 10 постов на странице.
+    paginator = Paginator(post_list, 10)
+    # извлечение № страницы из URL - значение параметра page.
+    page_number = request.GET.get('page')
+    # получение набора записей с нужной страницы.
+    page_obj = paginator.get_page(page_number)
+    title = 'Последние обновления!'
     template = 'posts/index.html'
+
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
         'title': title,
     }
+
     return render(request, template, context)
 
 
 def group_posts(request, slug):
+
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by('-pub_date')[:QUANTITY_OF_POSTS]
+    group_list = group.posts.all().order_by('-pub_date')
+    paginator = Paginator(group_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
+
     return render(request, template, context)
