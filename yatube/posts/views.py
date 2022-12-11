bc69c41305eb22f9a654eae527425eea29cf1c4e
Кирилл Черный <kirillcerny@gmail.com>
2022-12-11 18:04:12 +0300

    Созданы: персональная страница пользователя и страница записи (поста)

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from .models import Post, Group
+from .models import Post, Group, User
 
-QUANTITY_OF_POSTS = 10
+Q_OF_POSTS = 10
 
 
 def index(request):
 
-    # получение списка постов в обратном порядке.
     post_list = Post.objects.all().order_by('-pub_date')
-    # по 10 постов на странице.
-    paginator = Paginator(post_list, 10)
-    # извлечение № страницы из URL - значение параметра page.
+    paginator = Paginator(post_list, Q_OF_POSTS)
     page_number = request.GET.get('page')
-    # получение набора записей с нужной страницы.
     page_obj = paginator.get_page(page_number)
     title = 'Последние обновления!'
     template = 'posts/index.html'
@@ -31,7 +27,7 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     group_list = group.posts.all().order_by('-pub_date')
-    paginator = Paginator(group_list, 10)
+    paginator = Paginator(group_list, Q_OF_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -40,4 +36,38 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
 
+    return render(request, template, context)
+
+
+def profile(request, username):
+
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=author)
+    post_count = Post.objects.filter(author=author).count()
+    paginator = Paginator(posts, Q_OF_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    title = (f'Профайл пользователя {author}')
+    template = 'posts/profile.html'
+
+    context = {
+        'page_obj': page_obj,
+        'title': title,
+        'author': author,
+        'posts': posts,
+        'post_count': post_count,
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+
+    post = get_object_or_404(Post, id=post_id)
+    template = 'posts/post_detail.html'
+    title = post.text[30]
+
+    context = {
+        'post': post,
+        'title': title
+    }
     return render(request, template, context)
